1c1475df1b2c9f8952326427cf66a86abfea12e2
Thomas Galliker <thomas@bluewin.ch>
2018-08-07 19:33:00 +0200

    Release 3.0.3

--- XmlSerializerHelper.Android/Properties/AssemblyInfo.cs
+++ XmlSerializerHelper.Android/Properties/AssemblyInfo.cs
@@ -25,6 +25,6 @@ using Android.App;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("3.0.2")]
-[assembly: AssemblyVersion("3.0.2")]
-[assembly: AssemblyFileVersion("3.0.2")]
+// [assembly: AssemblyVersion("3.0.3")]
+[assembly: AssemblyVersion("3.0.3")]
+[assembly: AssemblyFileVersion("3.0.3")]
